1cb0ca0d8f7a7972c822c25115ba1cf975be839b
Ivan kravtsov <kravcneger@gmail.com>
2023-07-18 04:13:16 +0600

    Add commonCalculator

--- calculators/common.rb
+++ calculators/common.rb
@@ -1,6 +1,15 @@
-class CommonCalculator << Calculator
+class CommonCalculator < Calculator
 
   def update_quality
-    raise MESS
+	if item.sell_in > 0
+		item.quality -= 1 if item.quality > 0
+	else
+	  if item.quality > 1
+        item.quality -= 2
+	  else
+	    item.quality = 0
+	  end
+	end
+	item.sell_in -= 1
   end
 end
