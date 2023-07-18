b71ef83ff58e37f541e7c63b12a9db6c48ae75c5
Ivan kravtsov <kravcneger@gmail.com>
2023-07-19 01:57:49 +0600

    Update indents

--- lib/calculators/common.rb
+++ lib/calculators/common.rb
@@ -3,11 +3,11 @@ require 'calculators/calculator'
 class CommonCalculator < Calculator
 
   def update_quality
-	if item.sell_in > 0
-	  change_quality_by(-1)
-	else
-	  change_quality_by(-2)
-	end
-	item.sell_in -= 1
+    if item.sell_in > 0
+      change_quality_by(-1)
+    else
+      change_quality_by(-2)
+    end
+    item.sell_in -= 1
   end
 end
--- lib/item_manager.rb
+++ lib/item_manager.rb
@@ -1,24 +1,24 @@
 class ItemManager
   def initialize
     @brands = {
-		"Sulfuras, Hand of Ragnaros" => SulfurasCalculator, 
-		"Aged Brie" => AgedBrieCalculator,
-		"Backstage passes to a TAFKAL80ETC concert" => BackStagePasses,
-		"Conjured" => ConjuredCalculator
-	}
-	@calculators_instances = {}
-	@common_calculator ||= CommonCalculator.new
+      "Sulfuras, Hand of Ragnaros" => SulfurasCalculator, 
+      "Aged Brie" => AgedBrieCalculator,
+      "Backstage passes to a TAFKAL80ETC concert" => BackStagePasses,
+      "Conjured" => ConjuredCalculator
+    }
+    @calculators_instances = {}
+    @common_calculator ||= CommonCalculator.new
   end
 
   def update_quality item
     calculator = @common_calculator
     @brands.each do |name,cal|
       if name == item.name
-		@calculators_instances[name] ||= cal.new
-		calculator = @calculators_instances[name]
+        @calculators_instances[name] ||= cal.new
+        calculator = @calculators_instances[name]
       end
     end
-	calculator.item = item
+    calculator.item = item
     calculator.update_quality
   end
 
