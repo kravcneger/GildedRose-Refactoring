845ba3d9b4f1188da08a2a540f0920ca517183ae
Ivan kravtsov <kravcneger@gmail.com>
2023-07-18 23:19:38 +0600

    Add tests for common item

--- spec/lib/gilded_rose_spec.rb
+++ spec/lib/gilded_rose_spec.rb
@@ -1,6 +1,27 @@
 require 'spec_helper'
 
 describe GildedRose do
+  describe "Simple item" do
+    before { GildedRose.new([item]).update_quality() }
+
+    context "sell_in not expired" do
+      let(:item){ Item.new("item",2,10) }
+      it { expect(item.quality).to eq 9 }
+      it { expect(item.sell_in).to eq 1 }
+    end
+
+    context "sell_in expired" do
+      let(:item){ Item.new("item",0,10) }
+      it { expect(item.quality).to eq 8 }
+      it { expect(item.sell_in).to eq -1 }
+    end
+
+    context "checking for negative" do
+      let(:item){ Item.new("item",0,0) }
+      it { expect(item.quality).to eq 0 }
+      it { expect(item.sell_in).to eq -1 }
+    end
+  end
 
   describe "Aged Brie" do
     before { GildedRose.new([item]).update_quality() }
