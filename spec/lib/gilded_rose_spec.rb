3e44f8ced3f9a902f554fa40256e109524dc6d86
Ivan kravtsov <kravcneger@gmail.com>
2023-07-18 23:13:32 +0600

    Add test for old code calculating Aged brief

--- spec/lib/gilded_rose_spec.rb
+++ spec/lib/gilded_rose_spec.rb
@@ -2,12 +2,35 @@ require 'spec_helper'
 
 describe GildedRose do
 
-  describe "#update_quality" do
-    it "does not change the name" do
-      items = [Item.new("foo", 0, 0)]
-      GildedRose.new(items).update_quality()
-      expect(items[0].name).to eq "fixme"
+  describe "Aged Brie" do
+    context "sell_in not expired" do
+      let(:item){ Item.new("Aged Brie",2,10) }
+      before { GildedRose.new([item]).update_quality() }
+      it { expect(item.quality).to eq 11 }
+      it { expect(item.sell_in).to eq 1 }
     end
+
+    context "sell_in expired" do
+      let(:item){ Item.new("Aged Brie",0,10) }
+      before { GildedRose.new([item]).update_quality() }
+      it { expect(item.quality).to eq 12 }
+      it { expect(item.sell_in).to eq -1 }
+    end
+
+    context "checking for negative" do
+      let(:item){ Item.new("Aged Brie",-0,0) }
+      before { GildedRose.new([item]).update_quality() }
+      it { expect(item.quality).to eq 2 }
+      it { expect(item.sell_in).to eq -1 }
+    end
+  
+    context "max quality" do
+      let(:item){ Item.new("Aged Brie",-1,50) }
+      before { GildedRose.new([item]).update_quality() }
+      it { expect(item.quality).to eq 50 }
+      it { expect(item.sell_in).to eq -2 }
+    end
+
   end
 
 end
