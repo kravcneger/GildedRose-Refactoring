91f53a20ddd6ebd264164f9757a3d9226c3d6dd4
Ivan kravtsov <kravcneger@gmail.com>
2023-07-18 23:22:55 +0600

    Add test to Backstage

--- spec/lib/gilded_rose_spec.rb
+++ spec/lib/gilded_rose_spec.rb
@@ -53,7 +53,34 @@ describe GildedRose do
       it { expect(item.quality).to eq 50 }
       it { expect(item.sell_in).to eq -2 }
     end
+  end
 
+  describe "Backstage passes to a TAFKAL80ETC concert" do
+    let(:name){'Backstage passes to a TAFKAL80ETC concert'}
+    before { GildedRose.new([item]).update_quality() }
+
+    context "sell_in not expired" do
+      let(:item){ Item.new(name,20,20) }
+      it { expect(item.quality).to eq 21 }
+      it { expect(item.sell_in).to eq 19 }
+    end
+
+    context "sell_in less than or equal 10" do
+      let(:item){ Item.new(name,10,10) }
+      it { expect(item.quality).to eq 12 }
+      it { expect(item.sell_in).to eq 9 }
+    end
+
+    context "sell_in less than or equal 5" do
+      let(:item){ Item.new(name,5,10) }
+      it { expect(item.quality).to eq 13 }
+      it { expect(item.sell_in).to eq 4 }
+    end
+
+    context "max quality" do
+      let(:item){ Item.new(name,10,50) }
+      it { expect(item.quality).to eq 50 }
+    end
   end
 
 end
